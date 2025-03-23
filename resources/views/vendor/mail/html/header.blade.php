47da073964bfd429cecd2dfd2b8f52a7392c5ae6
WafiqSyahmi <23ftt1128@student.pb.edu.bn>
2025-03-23 23:02:32 +0800

    logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="pblogo.png" class="logo" alt="Laravel Logo">
+<img src="{{ asset('pblogo.png') }}" class="logo" alt="Your Logo">
 @else
 {{ $slot }}
 @endif
